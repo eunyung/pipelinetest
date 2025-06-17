b498d3888c185d43ea4db3501030e820eeeb56f0
eunyung <89629822+eunyung@users.noreply.github.com>
2025-06-17 10:41:39 +0900

    Update HellospringApplication.java

--- src/main/java/com/example/hellospring/HellospringApplication.java
+++ src/main/java/com/example/hellospring/HellospringApplication.java
@@ -1,1 +1,13 @@
-d
+package com.example.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringApplication.class, args);
+    }
+
+}
